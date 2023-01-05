cbeccd5b4a94a9edffefd66eeaf2accc2fa9344a
Diego Araujo <diego_shakan@hotmail.com>
2023-01-05 15:06:35 -0300

    fix tests after add devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "furiosa#{n}@example.com" }
+    password { "changeme" }
   end
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
     it "is success" do
+      user = create(:user)
+      sign_in user
       get root_path
       expect(response).to have_http_status(:success)
     end
