a7541dafcaed03c2a31fe0048a261c16418b51e7
Diego Araujo <diego_shakan@hotmail.com>
2023-01-05 11:27:47 -0300

    add tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   get 'home/index'
-
-
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
+    it "is success" do
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
