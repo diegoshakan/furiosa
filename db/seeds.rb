a31914a4063bb03e3864b1da58d9b6cfa23718eb
Diego Araujo <diego_shakan@hotmail.com>
2023-01-26 13:10:55 -0300

    index from all announcements, seeds, fix factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(first_name: "Diego", last_name: "Araujo", email: "diego@example.com", password: "changeme",
+user1 = User.create(first_name: "Diego", last_name: "Araujo", email: "diego@example.com", password: "changeme",
             nickname: "Dieguinho", gender: "male", phone: "84999991111")
+
+user2 = User.create(first_name: "Gandalf", last_name: "Cinzento", email: "user@example.com", password: "changeme",
+            nickname: "Mithrandir", gender: "male", phone: "84999991111")
 
 category_happy_party = Category.create(name: "Festa de Aniversário")
 Subcategory.create(name: "Criança", category: category_happy_party)
@@ -16,3 +19,6 @@ Subcategory.create(name: "Adulto", category: category_happy_party)
 category_wedding_party = Category.create(name: "Festa de Casamento")
 Subcategory.create(name: "Bolos", category: category_wedding_party)
 Subcategory.create(name: "Ornamentação", category: category_wedding_party)
+
+Announcement.create(title: "Xbox", description: "Game da geração passada", value: "1500.00", user: user1, category: category_happy_party)
+Announcement.create(title: "Playstation", description: "Game da geração passada", value: "1600.00", user: user2, category: category_wedding_party)
