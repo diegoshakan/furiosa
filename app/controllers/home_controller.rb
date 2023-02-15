c159573258d4424c6fc3cfc449add12e6e9e226d
Diego Araujo <diego_shakan@hotmail.com>
2023-02-15 11:48:12 -0300

    become the performance better with bullet gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    @announcements = Announcement.all
+    @announcements = Announcement.all_announcements
   end
 end
