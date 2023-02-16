f0e9610f5155e8d4960c1b3d867ded01dd35d022
Diego Araujo <diego_shakan@hotmail.com>
2023-02-15 21:08:31 -0300

    test

--- spec/requests/profile_spec.rb
+++ spec/requests/profile_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Profiles", type: :request do
+  let(:user){ FactoryBot.create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe "GET /show" do
     it "returns http success" do
-      get "/profile/show"
+      get "/profile"
       expect(response).to have_http_status(:success)
     end
   end
