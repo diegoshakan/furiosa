3b27631792aba7467f8271aed7239954959c3a78
Diego Araujo <diego_shakan@hotmail.com>
2024-07-13 18:50:45 -0300

    fix locale far away

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module Furiosa
     # config.eager_load_paths << Rails.root.join("extras")
     
     # I18n configurations
-    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [:"pt-BR", :en]
+    config.i18n.fallbacks = { :"pt-BR" => :en }
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
