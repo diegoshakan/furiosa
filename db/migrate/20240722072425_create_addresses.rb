2823712d6ec120d628f6fd2678124b426c0d2f6a
Diego Araujo <diego_shakan@hotmail.com>
2024-07-25 21:11:29 -0300

    adjustments to address

--- db/migrate/20240722072425_create_addresses.rb
+++ db/migrate/20240722072425_create_addresses.rb
@@ -1,14 +1,16 @@
 class CreateAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :addresses, id: :uuid do |t|
-      t.string :cep
-      t.string :rua
-      t.string :numero
-      t.string :complemento
-      t.string :ponto_de_referencia
-      t.string :uf
-      t.string :cidade
+      t.string :city, null: false
+      t.string :complement
+      t.string :number
+      t.string :reference_point
+      t.string :street, null: false
+      t.string :uf, null: false
+      t.string :zip_code, null: false
       t.references :user, null: false, foreign_key: true, type: :uuid
+
+      t.datetime :deleted_at, index: true
 
       t.timestamps
     end
